811c6690475f18eeb6e6deca75c4269cb58a9399
Paranchai <paranchai.wt@gmail.com>
2023-12-08 17:45:30 +0700

    2620. Counter

---
+++ 30 Days of JavaScript/Closures/2620. Counter.js
@@ -0,0 +1,11 @@
+var createCounter = function (n) {
+    let count = n
+    return function () {
+        return count++
+    }
+}
+
+const counter = createCounter(10)
+console.log(counter()) // 10
+console.log(counter()) // 11
+console.log(counter()) // 12
